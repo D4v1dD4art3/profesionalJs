55e7be90cc22e5891340c3924bf4db907447786b
David Duarte <jesusdavidduartejimenez@gmail.com>
2021-02-17 01:04:17 -0500

    firts template

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,7 @@ import MediaPlayer from "./MediaPlayer.js";
 import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
-const button = document.querySelector("button");
+const button = document.getElementById("buttonPaused");
 const isMuted = document.getElementById("isMuted");
 const player = new MediaPlayer({
   el: video,
