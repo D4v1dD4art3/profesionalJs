b06ccb08410bfb328438ed353f5bea0ce4afe0c8
David Duarte <jesusdavidduartejimenez@gmail.com>
2021-02-17 07:41:21 -0500

    efect parallax

--- assets/excercise/dunctiontoCallThis.js
+++ assets/excercise/dunctiontoCallThis.js
@@ -1,8 +1,14 @@
 //call se dice quien es el objecto contexto
+const david = {
+  name: "David",
+  apellido: "Duarte Jimenez",
+};
 function saludar() {
   console.log(`Hola soy ${this.name} ${this.apellido}`);
 }
 saludar.call(david);
+
+// call with arguments
 const david = {
   name: "David",
   apellido: "Duarte Jimenez",
@@ -12,8 +18,18 @@ function caminar(metros, direccion) {
   console.log(`${this.name} camina ${metros} metros hacia ${direccion}`);
 }
 caminar.call(david, 400, "norte");
+
 //establece 'this' usando apply
+const david = {
+  name: "David",
+  apellido: "Duarte Jimenez",
+};
 const valores = [400, "sur"];
+
+function caminar(metros, direccion) {
+  console.log(`${this.name} camina ${metros} metros hacia ${direccion}`);
+}
+
 caminar.apply(david, valores);
 
 // call, comas: cc
